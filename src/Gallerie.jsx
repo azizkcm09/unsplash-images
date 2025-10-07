8ebaee4aba4e0087bb81bc1404c99449865ad0e3
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-10-07 22:50:40 +0100

    rendering images

--- src/Gallerie.jsx
+++ src/Gallerie.jsx
@@ -1,7 +1,54 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
-
+const url =
+  "https://api.unsplash.com/search/photos?client_id=FW46CZLGjuOU93iYU_YbPTZEuq0W6ZLGOrpoJtcQ5Rc&query=office";
 const Gallerie = () => {
-  return <h2>Gallerie</h2>;
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data;
+    },
+  });
+  if (response.isLoading) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className="image-container">
+        <h4>Error...</h4>
+      </section>
+    );
+  }
+
+  const results = response.data.results;
+  if (results.length < 1) {
+    return (
+      <section className="image-container">
+        <h4>No images found</h4>
+      </section>
+    );
+  }
+  return (
+    <section className="image-container">
+      {results.map((item) => {
+        const url = item?.urls?.regular;
+        return (
+          <img
+            key={item.id}
+            src={url}
+            className="img"
+            alt={item.alt_description}
+          />
+        );
+      })}
+    </section>
+  );
 };
 
 export default Gallerie;
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css";
 import { AppProvider } from "./context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { QueryClient } from "../node_modules/@tanstack/query-core/src/queryClient";
+
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
