3e4c899e3d337a28dd2546321d71afd45318d939
aziz kacem <155546839+azizkcm09@users.noreply.github.com>
2025-10-07 18:27:58 +0100

    search Form

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const SearchForm = () => {
-  return <h2>SearchForm</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log("searching for:", searchValue);
+  };
+  return (
+    <section>
+      <h1 className="title"> unsplash images</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
